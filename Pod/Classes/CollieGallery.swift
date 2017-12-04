d15f45b5ce4d599b316c1af4d62cb08a4f24ed00
Ganesh Manickam <ganesh@nexialabs.com>
2017-12-04 18:03:39 +0530

    Close and Action button y axis padding added

--- Pod/Classes/CollieGallery.swift
+++ Pod/Classes/CollieGallery.swift
@@ -504,11 +504,11 @@ open class CollieGallery: UIViewController, UIScrollViewDelegate, CollieGalleryV
     }
     
     fileprivate func getCloseButtonFrame(_ avaiableSize: CGSize) -> CGRect {
-        return CGRect(x: 0, y: 0, width: 50, height: 50)
+        return CGRect(x: 0, y: 10, width: 50, height: 50)
     }
     
     fileprivate func getActionButtonFrame(_ avaiableSize: CGSize) -> CGRect {
-        return CGRect(x: avaiableSize.width - 50, y: 0, width: 50, height: 50)
+        return CGRect(x: avaiableSize.width - 50, y: 10, width: 50, height: 50)
     }
     
     fileprivate func getCustomButtonFrame(_ avaiableSize: CGSize, forIndex index: Int) -> CGRect {
