e9d1be65857b2848f64743000c6353e4be3e4d8b
Dielson Sales <dielson@ilhasoft.com.br>
2019-05-09 14:24:32 -0300

    Remove Pods folder

--- Example/Pods/Target Support Files/CollieGallery-iOS10.0/CollieGallery-iOS10.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CollieGallery_iOS10_0 : NSObject
-@end
-@implementation PodsDummy_CollieGallery_iOS10_0
-@end
--- Example/Pods/Target Support Files/CollieGallery-iOS8.3/CollieGallery-iOS8.3-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CollieGallery_iOS8_3 : NSObject
-@end
-@implementation PodsDummy_CollieGallery_iOS8_3
-@end
